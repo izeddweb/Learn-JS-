295df923050f49a9a82770db52381b0642bb6bc7
izeddweb <abzedd61@gmail.com>
2024-01-16 21:39:31 +0100

    new week assignment

---
+++ Assignment Week 2/app.js
@@ -0,0 +1,77 @@
+//.     $$$$$$$$$$.  Assignùent 1 
+
+// Add Variables Here
+let numberOne = 10;
+let numberTwo = 20;
+
+// Ouput
+console.log(numberOne +""+numberTwo); // Normal Concatenate => 1020
+console.log(typeof (numberOne+""+numberTwo)); // Normal Concatenate => String
+console.log(` ${numberOne}${numberTwo}`); // Template Literals Way => 1020
+console.log( typeof ` ${numberOne}${numberTwo}`); // Template Literals Way => String
+
+console.log(numberTwo+ "\n"+numberOne);
+
+/*
+Normal Concatenate
+20
+10
+*/
+
+console.log(`${numberTwo} \n${numberOne}`);
+/*
+Template Literals Way
+20
+10
+*/
+
+//.     $$$$$$$$$$.  Assignùent 2
+
+// console.log(elzero.innerHTML); // object
+// console.log(typeof elzero); // object
+
+//.     $$$$$$$$$$.  Assignùent 3
+
+console.log(" `I'm In \n \
+\\\\ \n \
+Love \\\ \"\"\" \'\'\'\ \n \
+++ With ++ \n \
+\\\"\"\"\\\"\"\"  \n \
+\"\"JavaScript\"\"`\   ");
+
+//.     $$$$$$$$$$.  Assignùent 4
+
+let a = 21;
+let b = 20;
+
+console.log("_"+`${a}_${b}`.repeat(4)+"_"); // _21_2021_2021_2021_20_  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
