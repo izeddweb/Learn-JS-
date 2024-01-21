b7933898020398f14110ed879ee90bee47972428
izeddweb <abzedd61@gmail.com>
2024-01-21 15:50:46 +0100

    add dir assgn week 3

---
+++ Assignment Week 2/challenge.js
@@ -0,0 +1,12 @@
+let titleCard = "zedd",descriptionCard = "zedd school",cardDate = "25/3" ;
+
+const template =
+`
+<div>
+    <h3> ${titleCard} </h3>
+    <p>${descriptionCard} </p>
+    <span> ${cardDate}</span>
+</div>
+
+`;
+document.write(template.repeat(4))
